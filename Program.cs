f6409389eebb6bc0315c8b2a35d7703bc69ac196
Snehasrg <sghongade642@gmail.com>
2023-01-02 13:47:50 +0530

    Welcome

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SnakeLadderGame
+{
+    class Program
+    {
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Welcome to Snake and Ladder Game");
+        }
+    }
+}
