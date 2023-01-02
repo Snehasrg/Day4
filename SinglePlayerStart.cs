fc9b9dc76c506236063156f1896a9fb4f64c9125
Snehasrg <sghongade642@gmail.com>
2023-01-02 13:57:23 +0530

    Start Game

---
+++ SinglePlayerStart.cs
@@ -0,0 +1,14 @@
+﻿namespace Snake_Ladder_Game
+{
+    internal class SinglePlayerStart
+    {
+        public static void  GameStart() 
+        {
+            int position = 0;
+            int player1;
+
+            player1 = position;
+            Console.WriteLine("Player 1 position is" + player1);
+        }
+    }
+}
