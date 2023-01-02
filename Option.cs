297a0bb8c3d0eb40dd692d6600efaa6705164d85
Snehasrg <sghongade642@gmail.com>
2023-01-02 17:07:09 +0530

    Checking option

---
+++ Option.cs
@@ -0,0 +1,54 @@
+﻿namespace Snake_Ladder_Game
+{
+    public class Option
+    {
+        public static void Checkoption()
+        {
+            int num;
+            int START_POSITION = 0;
+            int END_POSITION = 100;
+            int playerPosition = 0;
+            int previousPlayerPosition = 0;
+            int updatedPlayerPosition = 0;
+            int countDiceRoll = 0;
+
+            void rollDice()
+            {
+                Random Y = new Random();
+                num = Y.Next(6);
+                option();
+            }
+            void option()
+            {
+                int NO_PLAY_OPTION = 0;
+                int LADDER_OPTION = 1;
+                int SNAKE_OPTION = 2;
+                Random R = new Random();
+                int select_option = R.Next(3);
+
+                if (select_option == NO_PLAY_OPTION)
+                {
+                    playerPosition = previousPlayerPosition;
+                    rollDice();
+                }
+                else if (select_option == LADDER_OPTION)
+                {
+                    updatedPlayerPosition = (previousPlayerPosition + num);
+                }
+                else
+                {
+                    updatedPlayerPosition = (previousPlayerPosition - num);
+                }
+            }
+
+            while (playerPosition >= START_POSITION && playerPosition <= END_POSITION)
+            {
+                rollDice();
+            }
+
+        }
+    }
+}
+        
+    
+
